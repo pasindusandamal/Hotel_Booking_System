f0b5a0575acab0fe4fd3586024f018ad57303e57
nirusha99 <nirushakumarasinghe99@gmail.com>
2024-01-08 01:33:26 +0530

    Changed RegistrationForm

--- src/frontend/js/RegisterForm.js
+++ src/frontend/js/RegisterForm.js
@@ -15,7 +15,7 @@ export default function RegistrationForm() {
     e.preventDefault();
 
     try {
-      let result = await fetch("https://hotel-booking-backend0-7b55c6656f75.herokuapp.com/registerUser", {
+      let result = await fetch(" https://hotel-booking-backend-5a73b0667830.herokuapp.com:5000/registerUser", {
         method: "post",
         body: JSON.stringify({
           username,
