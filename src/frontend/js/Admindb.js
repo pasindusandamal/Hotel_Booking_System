7ca5ab387a02a98ade969a09eb2deea213fb3ce3
nirusha99 <nirushakumarasinghe99@gmail.com>
2024-01-07 19:44:50 +0530

    Chnaged url

--- src/frontend/js/Admindb.js
+++ src/frontend/js/Admindb.js
@@ -24,7 +24,7 @@ const Admindb = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/getUsers")
+      .get("https://hotel-booking-backend-5f327caf5929.herokuapp.com:5000/getUsers")
       .then((response) => {
         setUsers(response.data);
         setReservedDates(
